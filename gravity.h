7b5273b9bf4498d1a584bdaea44a34deffabe4d2
Karma Riuk <riukkarma@gmail.com>
2023-05-17 13:30:05 +0200

    Added gravity vector for polygons in gravity.h

--- gravity.h
+++ gravity.h
@@ -2,11 +2,13 @@
 #define GRAVITY_H_INCLUDED
 
 #include "balls.h"
+#include "polygons.h"
 
 extern void gravity_constant_field(double x, double y);
 extern void gravity_newton_field(double r, double g);
 
 extern vec2d gravity_vector(const ball* b);
+extern vec2d gravity_vector(const polygon* b);
 
 extern void gravity_draw_visible_field(cairo_t* cr);
 
